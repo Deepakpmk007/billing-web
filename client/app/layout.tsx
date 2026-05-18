36baea33328b23b398ea47b9fbefeb9408569c9b
Deepakpmk007 <deepakpmk@hotmail.com>
2026-05-18 19:39:43 +0530

    remove default tags

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import SideNavBar from "@/components/SideNavBar";
+import MainBar from "@/components/MainBar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,11 +25,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className={`h-full w-screen antialiased `}>
+      <body className="flex">
+        <div>
+          <SideNavBar />
+        </div>
+        <div className="flex flex-col w-full">
+          <MainBar />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
