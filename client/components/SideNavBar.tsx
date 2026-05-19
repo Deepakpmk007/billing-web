9b45cfb1b7b15231d057a3a5a8957797db3e0ecc
Deepakpmk007 <deepakpmk@hotmail.com>
2026-05-19 08:42:03 +0530

    chage some color

--- client/components/SideNavBar.tsx
+++ client/components/SideNavBar.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 export default function SideNavBar() {
   return (
-    <nav className="side_bar bg-blue-950 h-screen w-52">
-      <ul className="list-none flex flex-col gap-3">
+    <nav className="side_bar bg-[#3B3C36] h-screen w-52">
+      <ul className="list-none flex flex-col gap-3 text-white">
         <li>🍽️ Breakfast & Tiffin</li>
         <li>🌾 Village Specials</li>
         <li>🫓 Parotta & Main</li>
